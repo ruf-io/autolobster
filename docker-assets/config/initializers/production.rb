c7be098e7fdc1796c21139bc5e50650e10c22886
ruf-io <andy@materialize.com>
2021-04-08 23:50:07 -0400

    update for prod

--- docker-assets/config/initializers/production.rb
+++ docker-assets/config/initializers/production.rb
@@ -6,6 +6,15 @@ class << Rails.application
   def name
     ENV["LOBSTER_SITE_NAME"]
   end
+
+  def allow_invitation_requests?
+    true
+  end
+
+  def allow_new_users_to_invite?
+    true
+  end
+  
 end
 
 Rails.application.routes.default_url_options[:host] = Rails.application.domain
